47279902c38c416b0c4f796350a84527c3be130e
Marving <96314887+MarvingMoreton@users.noreply.github.com>
2022-07-04 11:40:24 -0400

    Create script.rb

---
+++ Ruby/8-kyu/are-you-playing-banjo/script.rb
@@ -0,0 +1,10 @@
+def are_you_playing_banjo(name)
+  # Implement me!
+  
+  if name.start_with? 'R', 'r'
+  return  name + " plays banjo"
+    
+    else
+    name + " does not play banjo"
+    end
+end
